41e5a84c8f0a19b8ff62208546b54772aedda415
nirholas <nirholas@users.noreply.github.com>
2026-03-06 03:24:30 +0000

    💧 cors.ts

--- packages/plugin.delivery/packages/gateway/src/cors.ts
+++ packages/plugin.delivery/packages/gateway/src/cors.ts
@@ -147,3 +147,4 @@ export default async function cors(req: Request, res: Response, options?: CorsOp
 export function initCors(options?: CorsOptions) {
   return (req: Request, res: Response) => cors(req, res, options);
 }
+
