23cffbc215447c0ba48cdbc0dc1a9e9647c6022b
nirholas <nirholas@users.noreply.github.com>
2026-03-06 03:24:35 +0000

    🗃️ const.ts

--- packages/plugin.delivery/scripts/openai-sync/const.ts
+++ packages/plugin.delivery/scripts/openai-sync/const.ts
@@ -3,3 +3,4 @@ import { resolve } from 'node:path';
 export const root = resolve(__dirname, '..');
 export const pluginDir = resolve(root, './public');
 export const BASE_URL = 'https://openai-collections.chat-plugin.sperax.com';
+
