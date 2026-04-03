2896c9b0d2063d6559cc9ae5e17342a8fc4358cc
tizilnaik03-cpu <tizilnaik03@gmail.com>
2026-04-03 21:23:53 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var TelegramBot = require('node-telegram-bot-api');
 var web3 = require('@solana/web3.js');
-
 var BOT_TOKEN = '8669635112:AAHb4lEJhUtUnm9wLAg4w8opND21La9op3E';
 var HELIUS_KEY = 'f783be12-4da4-4170-b5e9-c7a1fd1c03bb';
 var MAX = 10;
